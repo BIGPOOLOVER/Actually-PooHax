d92a7ea3b62db3f4d713432893d3fdd6bcd920c3
big poo <146683605+BIGPOOLOVER@users.noreply.github.com>
2024-07-23 00:44:00 -0700

    Update glassBridge.lua

--- Games/Squid Game Glass Bridge/glassBridge.lua
+++ Games/Squid Game Glass Bridge/glassBridge.lua
@@ -1,10 +1,8 @@
-local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/SiriusSoftwareLtd/Rayfield/main/source.lua'))()
-
 while true do
     local player = game.Players.LocalPlayer.Character.HumanoidRootPart
 local win = CFrame.new(-738, 73, -549)
 player.CFrame = win
 wait(4)
-    if CurrentValue = false then
-        break
+if CurrentValue = false then
+    break
 end
