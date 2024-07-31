e4c51ccb810106fd4e86b417c94cc6986081bfc5
big poo <146683605+BIGPOOLOVER@users.noreply.github.com>
2024-07-31 04:56:31 -0700

    Update glassBridge.lua

--- Games/Squid Game Glass Bridge/glassBridge.lua
+++ Games/Squid Game Glass Bridge/glassBridge.lua
@@ -1,8 +1,10 @@
-while true do
-                local plr = game.Players.LocalPlayer.Character.HumanoidRootPart
-                local autofarmgb = CFrame.new(-738, 73, -549 )
-                plr.CFrame = autofarmgb
-                if _Value == false then
-                    break
-                    else wait(4)
-                end
+Default = true
+
+while Default do
+    local plr = game.Players.LocalPlayer.Character.HumanoidRootPart
+    local autofarmgb = CFrame.new(-738, 73, -549)
+    plr.CFrame = autofarmgb
+    if Default == false then
+        break
+    end
+end
