9411b58970c6dae2d74fb722bf12a56b3b48e375
big poo <146683605+BIGPOOLOVER@users.noreply.github.com>
2024-07-31 01:24:13 -0700

    Update glassBridge.lua

--- Games/Squid Game Glass Bridge/glassBridge.lua
+++ Games/Squid Game Glass Bridge/glassBridge.lua
@@ -1,9 +1,22 @@
-if Value == true then
-            while true do
-                local plr = game.Players.LocalPlayer.Character.HumanoidRootPart
-                local autofarmgb = CFrame.new(-738, 73, -549 )
-                plr.CFrame = autofarmgb
-                if _Value == false then
-                    break
-                    else wait(4)
-                end
+-- local Toggle = Tab:CreateToggle({
+   -- Name = "Toggle Example",
+   -- CurrentValue = false,
+   -- Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+  -- Callback = function(Value)
+   -- The function that takes place when the toggle is pressed
+   -- The variable (Value) is a boolean on whether the toggle is true or false
+  -- end,
+-- })
+
+-- test 
+
+local CurrentValue = true
+
+while wait(4) do
+    if CurrentValue then
+        print('hi')
+    else
+        print('shutting off script')
+        break
+    end
+end
